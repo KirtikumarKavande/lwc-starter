da1d1a06b5b7593849b8f63895fac347e27a0a90
KirtikumarKavande <kirtikumar0005@gmail.com>
2025-05-06 16:09:42 +0530

    iterator in lwc

--- force-app/main/default/lwc/demoStarterProject/demoStarterProject.js
+++ force-app/main/default/lwc/demoStarterProject/demoStarterProject.js
@@ -2,9 +2,10 @@ import { LightningElement, track } from 'lwc';
 
 export default class DemoStarterProject extends LightningElement {
     
-    isShow=false
-    typeInsideInput(e){
-        this.isShow=e.target.value.length
-        // we know 0 is false value
-    }
+    items = [
+        { id: '1', name: 'John Doe', email: 'john.doe@example.com' },
+        { id: '2', name: 'Jane Smith', email: 'jane.smith@example.com' },
+        { id: '3', name: 'Robert Johnson', email: 'robert.johnson@example.com' }
+    ]
+
 }
