6d183c9e1771323001f662e6f243113bb5f6554b
KirtikumarKavande <kirtikumar0005@gmail.com>
2025-05-06 11:54:58 +0530

    data binding between js and html  js to html

--- force-app/main/default/lwc/demoStarterProject/demoStarterProject.js
+++ force-app/main/default/lwc/demoStarterProject/demoStarterProject.js
@@ -1,3 +1,6 @@
 import { LightningElement } from 'lwc';
 
-export default class DemoStarterProject extends LightningElement {}
+export default class DemoStarterProject extends LightningElement {
+    name="kirtikumar Kavande" 
+    // objects and primitive types can accessed inside html(view) we can not access array 
+}
