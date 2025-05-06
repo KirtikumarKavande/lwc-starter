5ce948fe3cccc4d160729bac5073a862b781d8eb
KirtikumarKavande <kirtikumar0005@gmail.com>
2025-05-06 14:42:50 +0530

    @track to track nested value

--- force-app/main/default/lwc/demoStarterProject/demoStarterProject.js
+++ force-app/main/default/lwc/demoStarterProject/demoStarterProject.js
@@ -1,9 +1,18 @@
-import { LightningElement } from 'lwc';
+import { LightningElement, track } from 'lwc';
 
 export default class DemoStarterProject extends LightningElement {
-    name="kirtikumar Kavande" 
-    designation="Developer"
-    handleChange(e){
-        this.designation=e.target.value
+    name = "kirtikumar Kavande"
+    designation = "Developer"
+    handleChange(e) {
+        this.designation = e.target.value
+    }
+    address = {
+        street: "123, ABC",
+        city: "PQR",
+        state: "XYZ",
+        zip: "111"
+    }
+    changeCity(e) {
+        this.address = { ...this.address, city: e.target.value }
     }
 }
