1533626f6b098528603fe381e91a0251df40c0c6
KirtikumarKavande <kirtikumar0005@gmail.com>
2025-05-07 10:15:13 +0530

    shared css

---
+++ force-app/main/default/lwc/anotehrDemoStarterProject/anotehrDemoStarterProject.js
@@ -0,0 +1,3 @@
+import { LightningElement } from 'lwc';
+
+export default class AnotehrDemoStarterProject extends LightningElement {}
--- force-app/main/default/lwc/parentHelloComponent/parentHelloComponent.js
+++
@@ -1,3 +0,0 @@
-import { LightningElement } from 'lwc';
-
-export default class ParentHelloComponent extends LightningElement {}
