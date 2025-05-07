44960fd80b77fbfd14674d046bf4b277fb6ca1e0
KirtikumarKavande <kirtikumar0005@gmail.com>
2025-05-07 09:46:23 +0530

    dynamic styling

--- force-app/main/default/lwc/demoStarterProject/demoStarterProject.js
+++ force-app/main/default/lwc/demoStarterProject/demoStarterProject.js
@@ -1,11 +1,12 @@
 import { LightningElement, track } from 'lwc';
 
 export default class DemoStarterProject extends LightningElement {
-    
-    items = [
-        { id: '1', name: 'John Doe', email: 'john.doe@example.com' },
-        { id: '2', name: 'Jane Smith', email: 'jane.smith@example.com' },
-        { id: '3', name: 'Robert Johnson', email: 'robert.johnson@example.com' }
-    ]
+    percentage = 10
+    handleInputChange(e) {
+        this.percentage = e.target.value
+    }
+    get boxStyle() {
+        return `width:${this.percentage}%`
 
+    }
 }
