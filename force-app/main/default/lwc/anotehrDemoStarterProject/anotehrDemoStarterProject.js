77a2ca5b738b9f57749dd3f63325127aa053aa26
KirtikumarKavande <kirtikumar0005@gmail.com>
2025-05-10 16:09:11 +0530

    lifecycle methods

--- force-app/main/default/lwc/anotehrDemoStarterProject/anotehrDemoStarterProject.js
+++ force-app/main/default/lwc/anotehrDemoStarterProject/anotehrDemoStarterProject.js
@@ -1,3 +1,22 @@
 import { LightningElement } from 'lwc';
 
-export default class AnotehrDemoStarterProject extends LightningElement {}
+export default class AnotehrDemoStarterProject extends LightningElement {
+    constructor(){
+        super()
+        console.log("Child component constructor rendered")
+     }
+    
+     connectedCallback(){
+        throw new Error("hey!! rendering of child component failed")
+        console.log("Child component connectedCallback rendered")
+    
+     }
+    
+     renderedCallback(){
+        console.log("Child component renderedCallback rendered")
+    
+     }
+     disconnectedCallback(){
+        console.log("disconnected callback from child")
+     }
+}
--- force-app/main/default/lwc/demoStarterProject/demoStarterProject.js
+++ force-app/main/default/lwc/demoStarterProject/demoStarterProject.js
@@ -1,12 +1,29 @@
 import { LightningElement, track } from 'lwc';
 
 export default class DemoStarterProject extends LightningElement {
-    percentage = 10
-    handleInputChange(e) {
-        this.percentage = e.target.value
-    }
-    get boxStyle() {
-        return `width:${this.percentage}%`
+ constructor(){
+    super()
+    console.log("parent component constructor rendered")
+ }
+ isShow=true
 
-    }
+ connectedCallback(){
+    console.log("parent component connectedCallback rendered")
+
+ }
+
+ renderedCallback(){
+    console.log("parent component renderedCallback rendered")
+
+ }
+errorCallback(err,stack){
+    console.log("what is error",err.message)
+    console.log("what is stack",stack)
+}
+
+
+ handleClick(){
+    this.isShow=false
+ }
+
 }
